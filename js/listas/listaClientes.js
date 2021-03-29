fc137f88d459fd00d213b5a1f8b35eb2728ba25d
Mateus Henrique <mateus55henrique@hotmail.com>
2021-03-29 12:31:01 -0300

    Filtro por nome/id na listagem de clientes

--- js/listas/listaClientes.js
+++ js/listas/listaClientes.js
@@ -1,139 +1,157 @@
-$('.dropdown-trigger').dropdown({
-    container: document.body
+$(".dropdown-trigger").dropdown({
+  container: document.body,
 });
 
-$(document).ready(function() {
-    $('.fixed-action-btn').floatingActionButton();
+$(document).ready(function () {
+  $(".fixed-action-btn").floatingActionButton();
 
-    $(".modal").modal();
+  $(".modal").modal();
 
-    $("select").formSelect();
+  $("select").formSelect();
 
-    $(".edita").click(function(){
-        $.ajax({
-            url: "../controller/select/dadosCliente.php",
-            type: "POST",
-            data: {
-                id: $(this).attr("data-id")
-            },
-            success: function(data) {
-                var retorno = $.parseJSON(data);
-                $("#nome_cliente").select();
-                $("#nome_cliente").val(retorno.nomecliente);
-                $("#telefone_cliente").select();
-                $("#telefone_cliente").val(retorno.telefonecliente);
-                $("#telefone_cliente").blur();
-                $("#setor_cliente").val( $('option:contains("'+retorno.nomesetor+'")').val() );
-                $("#corredor_cliente").val( $('option:contains("'+retorno.nomecorredor+'")').val());
-                $("select").formSelect();
-                if(retorno.ativo){
-                    $("#ativo").attr("checked", 'checked');
-                }else{
-                    $("#ativo").removeAttr("checked");
-                }
-                $("#form_edita").attr("data-id", retorno.idcliente);
-            }
-        });
+  $("#filtro").submit(function (e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: "../controller/select/filtros/clientes.php",
+      type: "POST",
+      data: {
+        filter: function () {
+          return $("#filter").val();
+        },
+      },
+      success: function (data) {
+        $("#lista_clientes").html("");
+        $("#lista_clientes").append(data);
+      },
+    });
+  });
+
+  $(".edita").click(function () {
+    $.ajax({
+      url: "../controller/select/dadosCliente.php",
+      type: "POST",
+      data: {
+        id: $(this).attr("data-id"),
+      },
+      success: function (data) {
+        var retorno = $.parseJSON(data);
+        $("#nome_cliente").select();
+        $("#nome_cliente").val(retorno.nomecliente);
+        $("#telefone_cliente").select();
+        $("#telefone_cliente").val(retorno.telefonecliente);
+        $("#telefone_cliente").blur();
+        $("#setor_cliente").val(
+          $('option:contains("' + retorno.nomesetor + '")').val()
+        );
+        $("#corredor_cliente").val(
+          $('option:contains("' + retorno.nomecorredor + '")').val()
+        );
+        $("select").formSelect();
+        if (retorno.ativo) {
+          $("#ativo").attr("checked", "checked");
+        } else {
+          $("#ativo").removeAttr("checked");
+        }
+        $("#form_edita").attr("data-id", retorno.idcliente);
+      },
+    });
+  });
+
+  $("#form_edita").submit(function () {
+    event.preventDefault();
+    var id = this.getAttribute("data-id");
+    var dados = new FormData(this);
+    dados.append("id", id);
+
+    // // Exibição dos valores chave/valor
+    // for (var pair of dados.entries()) {
+    //     console.log(pair[0] + ', ' + pair[1]);
+    // }
+
+    $.ajax({
+      url: "../controller/update/atualizaCliente.php",
+      type: "POST",
+      data: dados,
+      processData: false,
+      contentType: false,
+      success: function (data) {
+        var retorno = $.parseJSON(data);
+        if (retorno.cod == "1") {
+          M.toast({
+            html: "Cliente atualizado!",
+            classes: "green",
+          });
+          location.reload();
+        } else {
+          M.toast({
+            html: "Erro ao atualizar!",
+            classes: "red",
+          });
+        }
+      },
+    });
+  });
+
+  $(".exclui").click(function () {
+    var id = $(this).attr("data-id");
+
+    $.ajax({
+      url: "../controller/select/dadosCliente.php",
+      type: "POST",
+      data: { id: $(this).attr("data-id") },
+      success: function (data) {
+        data = $.parseJSON(data);
+        $("#confirma_exclui_button").attr("data-id", data.idcliente);
+        $("#nome_exclui").text(data.nomecliente);
+      },
+    });
+  });
+
+  $("#confirma_exclui_button").click(function (e) {
+    $.ajax({
+      url: "../controller/delete/excluiCliente.php",
+      type: "POST",
+      data: { id: $(this).attr("data-id") },
+      success: function (data) {
+        var retorno = $.parseJSON(data);
+        if (retorno.cod == "1") {
+          M.toast({
+            html: "Cliente excluído!",
+            classes: "green",
+          });
+          location.reload();
+        } else {
+          M.toast({
+            html: "Erro ao excluir!",
+            classes: "red",
+          });
+        }
+      },
+    });
+  });
+
+  $(".carregar_mais").click(function () {
+    var inicio = $("tbody tr").length - 1;
+
+    $.ajax({
+      url: "../controller/select/carregaMaisClientes.php",
+      type: "POST",
+      dataType: "html",
+      data: { inicio: inicio },
+      success: function (data) {
+        $(".carregar_mais").remove();
+        $("#lista_clientes").append(data);
+      },
     });
 
-    $("#form_edita").submit(function() {
-        event.preventDefault();
-        var id = this.getAttribute("data-id");
-        var dados = new FormData(this);
-        dados.append('id', id);
+    return false;
+  });
 
-        // // Exibição dos valores chave/valor
-        // for (var pair of dados.entries()) {
-        //     console.log(pair[0] + ', ' + pair[1]);
-        // }
+  $("#top").click(function () {
+    //scroll suave
+    $("html, body").animate({ scrollTop: 0 }, "slow"); //slow, medium, fast
+  });
 
-        $.ajax({
-            url: "../controller/update/atualizaCliente.php",
-            type: "POST",
-            data: dados,
-            processData: false,
-            contentType: false,
-            success: function(data) {
-                var retorno = $.parseJSON(data);
-                if (retorno.cod == '1') {
-                    M.toast({
-                        html: 'Cliente atualizado!',
-                        classes: 'green'
-                    });
-                    location.reload();
-                } else {
-                    M.toast({
-                        html: 'Erro ao atualizar!',
-                        classes: 'red'
-                    });
-                }
-            }
-        });
-    });
-
-    $(".exclui").click(function(){
-        var id = $(this).attr("data-id");
-
-        $.ajax({
-            url: "../controller/select/dadosCliente.php",
-            type: "POST",
-            data: {id: $(this).attr("data-id")},
-            success: (function(data) {
-                data = $.parseJSON(data);
-                $("#confirma_exclui_button").attr("data-id", data.idcliente);
-                $("#nome_exclui").text(data.nomecliente);
-            })
-        });
-    });
-
-    $("#confirma_exclui_button").click(function(e){
-        $.ajax({
-            url: "../controller/delete/excluiCliente.php",
-            type: "POST",
-            data: {'id': $(this).attr("data-id")},
-            success: function(data){
-                var retorno = $.parseJSON(data);
-                if (retorno.cod == '1') {
-                    M.toast({
-                        html: 'Cliente excluído!',
-                        classes: 'green'
-                    });
-                    location.reload();
-                } else {
-                    M.toast({
-                        html: 'Erro ao excluir!',
-                        classes: 'red'
-                    });
-                }
-            }
-        });
-    });
-
-    $(".carregar_mais").click(function() {
-        var inicio = $("tbody tr").length - 1;
-
-        $.ajax({
-
-            url: '../controller/select/carregaMaisClientes.php',
-            type: 'POST',
-            dataType: 'html',
-            data: {'inicio': inicio},
-            success: function(data) {
-                $(".carregar_mais").remove();
-                $('#lista_clientes').append(data);
-            }
-        });
-
-        return false;
-    });
-
-    
-
-    $('#top').click(function(){
-        //scroll suave
-        $('html, body').animate({scrollTop:0}, 'slow'); //slow, medium, fast
-    })
-
-    $('.fixed-action-btn').floatingActionButton();
-
-})
+  $(".fixed-action-btn").floatingActionButton();
+});
---
+++ php/controller/select/filtros/clientes.php
@@ -0,0 +1,135 @@
+<?php
+    include_once "../../../config/conexao.php";
+
+    $filter = '%'.$_POST['filter'].'%';
+
+    $sqlSelecionaClientes = $connect->prepare("SELECT tabcliente.corredorcliente, tabcliente.idsetorcliente, tabcliente.ativo, tabcliente.idcliente, tabcliente.nomecliente, tabcliente.telefonecliente, tabsetor.nomesetor, tabcorredor.nomecorredor
+                                                FROM tabcliente
+                                            INNER JOIN tabsetor ON tabcliente.idsetorcliente = tabsetor.idsetor
+                                            INNER JOIN tabcorredor ON tabcorredor.idcorredor = tabcliente.corredorcliente
+                                            WHERE tabcliente.nomecliente LIKE ? OR tabcliente.idcliente LIKE ?
+                                            ORDER BY tabcliente.datacadastrocliente DESC");
+    $sqlSelecionaClientes->bind_param("ss", $filter, $filter);
+    $sqlSelecionaClientes->execute();
+
+    $resultClientes = $sqlSelecionaClientes->get_result();
+
+    if ($resultClientes->num_rows == 0) {
+    ?>
+        <tr>
+            <td colspan='7'>Nenhum registro encontrado...</td>
+        </tr>
+        <?php
+    } else {
+        while ($resClientes = $resultClientes->fetch_assoc()) {
+        ?>
+            <tr>
+                <td><?= $resClientes['idcliente'] ?></td>
+                <td><?= $resClientes['nomecliente'] ?></td>
+                <td><?= $resClientes['telefonecliente'] ?></td>
+                <td><?= $resClientes['nomesetor'] ?></td>
+                <td><?= $resClientes['nomecorredor'] ?></td>
+                <td>
+                    <button data-target="edita" data-id="<?= $resClientes['idcliente'] ?>" class="btn-small white orange-text text-darken-3 modal-trigger edita">
+                        <i class="material-icons">create</i>
+                    </button>
+                    <button data-target="exclui" data-id="<?= $resClientes['idcliente'] ?>" class="btn-small white red-text text-darken-3 modal-trigger exclui">
+                        <i class="material-icons">delete</i>
+                    </button>
+                </td>
+            </tr>
+        <?php
+        }
+    }
+    ?>
+    <!-- MODAL EDITA -->
+    <div class='modal fade' id='edita'>
+        <div class='modal-content'>
+            <div class='row right'><i class='material-icons modal-close'>close</i></div>
+            <h4>Editar informações</h4>
+            <div class='divider'></div>
+            <form method='post' action='#' id='form_edita'>
+                <div class="row">
+                    <div class="input-field col l6">
+                        <input id="nome_cliente" type="text" class="validate" name='nome_cliente' onkeyup="this.value = this.value.toUpperCase();">
+                        <label for="nome_cliente">Nome completo</label>
+                    </div>
+                    <div class="input-field col l6">
+                        <input id="telefone_cliente" type="text" class="validate" name='telefone_cliente'>
+                        <label for="telefone_cliente">Telefone</label>
+                    </div>
+                    <div class="input-field col l3">
+                        <select name='setor_cliente' id='setor_cliente'>
+                            <?php
+                            $sqlSetor = $connect->prepare('SELECT * FROM tabsetor');
+                            $sqlSetor->execute();
+
+                            $resultSetor = $sqlSetor->get_result();
+
+                            while ($resSetor = $resultSetor->fetch_assoc()) {
+                            ?>
+                                <option value="<?= $resSetor['idsetor'] ?>"><?= $resSetor['nomesetor'] ?></option>
+                            <?php
+                            }
+
+
+                            ?>
+                        </select>
+                        <label>Setor</label>
+                    </div>
+                    <div class="input-field col l3">
+                        <select name='corredor_cliente' id='corredor_cliente'>
+                            <?php
+                            $sqlUnid = $connect->prepare('SELECT * FROM tabcorredor');
+                            $sqlUnid->execute();
+
+                            $resultUnid = $sqlUnid->get_result();
+
+                            while ($resUnid = $resultUnid->fetch_assoc()) {
+                            ?>
+                                <option value="<?= $resUnid['idcorredor'] ?>"><?= $resUnid['nomecorredor'] ?></option>
+                            <?php
+                            }
+                            ?>
+                        </select>
+                        <label>Corredor</label>
+                    </div>
+                    <div class='col l3'>
+                        <p>
+                            <label>
+                                <input type="checkbox" class="filled-in" name='ativo' id="ativo" />
+                                <span>Ativo</span>
+                            </label>
+                        </p>
+                    </div>
+                </div>
+                <div class="row center">
+                    <button class='btn light-blue darken-3 botao' type='submit' name='action' data-id="" id="enviar">
+                        Atualizar
+                        <i class='material-icons right'>check</i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- MODAL EXCLUI -->
+    <div id="exclui" class="modal">
+        <div class="modal-content">
+            <i class='material-icons right modal-close' id='fechar_modal'>close</i>
+            <h5>Confirma que deseja excluir <span id="nome_exclui"></span></h5>
+            <p class="red-text">ATENÇÃO: Esta ação não poderá ser revertida posteriormente!</p>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat green-text" id="confirma_exclui_button" data-id="">Confirma</a>
+            <a href="" class="modal-close waves-effect waves-red btn-flat red-text">Cancela</a>
+        </div>
+    </div>
+    <div class="fixed-action-btn" id='top'>
+        <a class="btn-floating btn-large orange darken-3">
+            <i class="large material-icons">keyboard_arrow_up</i>
+        </a>
+    </div>
+    <script src="../../js/listas/listaClientes.js"></script>
+
+?>
